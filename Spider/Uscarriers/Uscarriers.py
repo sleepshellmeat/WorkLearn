27422ab94b43b5fe106ed3ba4ef6c49088c7cd28
rong <www.429429@gmail.com>
2019-03-19 22:11:18 +0800

    知道了Xpath的使用方法，pase换为HTML

--- Spider/Uscarriers/Uscarriers.py
+++ Spider/Uscarriers/Uscarriers.py
@@ -1,30 +1,33 @@
 import requests
 from lxml import etree
+from time import sleep
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36}'
 }
+start_url = 'http://www.uscarriers.net/'
 
 # 定义一个解析首页的函数，把CVN、DDG、CG、等解析后的url返回一个列表，以备二次解析。
 def parse_navy_all():
-    start_url = 'http://www.uscarriers.net/'
     r = requests.get(start_url, headers=headers)
     # 创建一个Xpath对象
-    navy_all = etree.parse(r.text)
+    navy_all = etree.HTML(r.text)
     # //tr//a[contains(text(),"USS")]/@href 解析二级页面的Xpath.
     all_href_list = navy_all.xpath('//tr//div[@align="center"]/a[1]/@href')
     return all_href_list
 
 def parse_second(all_href_list):
     for href in all_href_list:
-        r = requests.get(href, headers)
-        second_all = etree.parse(r.text)
+        r = requests.get(start_url + href, headers)
+        second_all = etree.HTML(r.text)
         second_href_list = second_all.xpath('//tr//a[contains(text(),"USS")]/@href')
-        return second_href_list
+        print(second_href_list)
+        sleep(3)
 
 def main():
     all_href_list = parse_navy_all()
     parse_second(all_href_list)
-
+#
 if __name__ == '__main__':
     main()
+
