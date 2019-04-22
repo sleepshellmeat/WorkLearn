763ea4706d9f8b89cd50c5fc3f9acf58a6e877b4
rong <www.429429@gmail.com>
2019-04-22 11:43:54 +0800

    添加了sleep和打印语句

--- Spider/first/executive/51job.py
+++ Spider/first/executive/51job.py
@@ -1,6 +1,5 @@
 import requests
 from time import sleep
-from lxml import etree
 from bs4 import BeautifulSoup
 
 
@@ -51,7 +50,9 @@ def main():
     for page in range(start_page, end_page + 1):
         url = pinjie_url(key_word, page, url)
         parse_url(url, fp)
-
+        print('正在爬取第%s页...' % page)
+        sleep(5)
+        print('结束爬取第%s页!' % page)
     fp.close()
 
 
